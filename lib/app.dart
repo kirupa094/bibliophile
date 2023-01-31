2c9abfd2dde0b4d96556082a5f553c75f43e624a
kiruba094 <kirupakaran094@gmail.com>
2023-01-31 20:09:32 +0530

    Home renamed as Login

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,4 @@
-import 'package:bibliophile/screen/Home/login_screen.dart';
+import 'package:bibliophile/screen/Login/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:responsive_framework/responsive_framework.dart';
