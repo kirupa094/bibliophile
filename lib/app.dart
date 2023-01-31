c1e77a66ea4bbcdff4f35bb6d1c77bfa0fe9debf
kiruba094 <kirupakaran094@gmail.com>
2023-01-31 16:48:55 +0530

    add responsive_framework

---
+++ lib/app.dart
@@ -0,0 +1,27 @@
+import 'package:bibliophile/screen/Home/login_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:responsive_framework/responsive_framework.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Bibliophile App',
+      theme: ThemeData(backgroundColor: Colors.white),
+      debugShowCheckedModeBanner: false,
+      builder: (context, child) => ResponsiveWrapper.builder(child,
+          maxWidth: 1200,
+          minWidth: 480,
+          defaultScale: true,
+          breakpoints: const [
+            ResponsiveBreakpoint.resize(480, name: MOBILE),
+            ResponsiveBreakpoint.autoScale(800, name: TABLET),
+            // ResponsiveBreakpoint.resize(1000, name: DESKTOP),
+            // ResponsiveBreakpoint.autoScale(2460, name: '4K'),
+          ]),
+      home: const LoginScreen(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,9 @@
-import 'package:bibliophile/screen/Home/login_screen.dart';
+import 'package:bibliophile/app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Bibliophile App',
-      theme: ThemeData(
-        backgroundColor: Colors.white
-      ),
-      debugShowCheckedModeBanner: false,
-      home: const LoginScreen(),
-    );
-  }
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  runApp(const App());
 }
