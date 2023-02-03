243b5cfd18e634a7e30bf2ea207c89610480ec2a
Samhan097 <48414384+Samhan097@users.noreply.github.com>
2023-02-03 10:19:16 +0530

    add sharedPreference Library

--- lib/bloc/bloc.dart
+++ lib/bloc/bloc.dart
@@ -1,3 +1,46 @@
+import 'package:bibliophile/resources/repository.dart';
+import 'package:rxdart/rxdart.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class InitData {
+  final String token;
+  InitData({
+    this.token = "",
+  });
+}
+
 class Bloc {
-  
+  final Repository _repository;
+  final BehaviorSubject<InitData> _initDataConfig;
+
+  String _token = "";
+
+  static final Bloc _bloc = Bloc._internal();
+
+  factory Bloc() {
+    return _bloc;
+  }
+
+  String getSavedUserToken() {
+    return _token;
+  }
+
+  setToken(String token) {
+    _token = token;
+  }
+
+  Bloc._internal()
+      : _repository = Repository(),
+        _initDataConfig = BehaviorSubject<InitData>();
+
+  getInitData() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    _initDataConfig.sink.add(InitData(
+      token: prefs.getString('token') ?? "",
+    ));
+
+    String? token = prefs.getString('token');
+
+    _token = token ?? '';
+  }
 }
